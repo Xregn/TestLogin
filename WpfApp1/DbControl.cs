556c05ad289493bfcf5acf9e2ed54fdca629e821
Nick <surve077@gmail.com>
2019-04-26 10:12:31 +0300

    Create 25.04.
    my second wpf

---
+++ WpfApp1/DbControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+
+namespace TestLogin
+{
+    class DbControl
+    {
+        private string connectionString = @"Data Source=DESKTOP-AI8VOIA\SQLEXPRESS;Integrated Security=false;;Initial Catalog=GameOfLifeDb;User ID=root; Password=123;";
+       public string connectString {
+            set { connectionString = value; } }
+
+        public SqlDataReader conf(string connectionString, string queryString)
+        {
+            this.connectionString = @"Data Source=DESKTOP-AI8VOIA\SQLEXPRESS;Integrated Security=false;;Initial Catalog=GameOfLifeDb;User ID=root; Password=123;";
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection = new SqlConnection(this.connectionString);
+            sqlConnection.Open();
+
+            SqlCommand command = new SqlCommand(queryString, sqlConnection);
+            SqlDataReader read = command.ExecuteReader();
+            read.Read();
+            
+            //while (read.Read())
+            //{ Console.WriteLine("{0}\t{1}", read.GetInt32(0), read.GetString(1));}
+
+            //sqlConnection.Close();
+            //sqlConnection.Dispose();
+
+              return read;
+        }
+
+    }
+    
+   
+}
---
+++ WpfApp1/LoginViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using System.Windows;
+
+namespace TestLogin
+{
+    public class LoginViewModel
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+
+        public LoginViewModel()
+        {
+            EnterCommand = new DelegateCommand(Enter);
+            CancelCommand = new DelegateCommand(Cancel);
+        }
+        public DelegateCommand EnterCommand { get; private set; }
+        public DelegateCommand CancelCommand { get; set; }
+        private void Enter(object obj)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+        private void Cancel(object obj)
+        {
+            MessageBox.Show("jnvyf");
+        }
+
+        
+    }
+
+    
+
+}
